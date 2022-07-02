8795cdebd298202c7d6ef6e942c7f359f68d92a3
Er <erickruiz1994@gmail.com>
2022-07-02 10:43:27 -0600

    Agregar archivos de proyecto.

---
+++ Models/CalculoCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CalculoCliente
+    {
+        public int id_calculo { get; set; }
+        public int id_cliente { get; set; }
+        public int id_rubro { get; set; }
+        public string impuesto { get; set; }
+        public string descripcion { get; set; }
+        public double total { get; set; }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Models
+{
+    public class Cliente
+    {
+        public int id_cliente { get; set; }
+        public int CUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public double Salario { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public string Ciudad { get; set; }
+        public int id_Pais { get; set; }
+        public int id_rubro { get; set; }
+    }
+}
